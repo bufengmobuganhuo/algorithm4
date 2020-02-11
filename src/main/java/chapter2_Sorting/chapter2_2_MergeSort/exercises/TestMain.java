08558f0e411f5b838f6964f975a6780d97543513
zy_1900 <18681820448@163.com>
2020-02-11 10:57:03 +0800

    #练习2.2.11&练习2.2.12

---
+++ src/main/java/chapter2_Sorting/chapter2_2_MergeSort/exercises/TestMain.java
@@ -0,0 +1,20 @@
+package chapter2_Sorting.chapter2_2_MergeSort.exercises;
+
+import chapter1_Fundamentals.utils.ArrayUtil;
+
+/**
+ * @author zhangyu
+ * 2020/2/10 16:59
+ * TODO
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        Integer[] arr= ArrayUtil.createInt(20,25);
+        EX_2_2_11_ImproveMerge quickMerge=new EX_2_2_11_ImproveMerge();
+        quickMerge.show("排序前：",arr);
+        quickMerge.sort(arr);
+        quickMerge.show("排序后：",arr);
+
+
+    }
+}
