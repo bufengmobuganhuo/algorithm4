94f6647d5009e13fce64dbd078f3c27a5387d252
yuzhang <18681820448@163.com>
2020-07-05 12:09:02 +0800

    #leetcode链表相关题目

---
+++ src/main/java/leetcode/linkedlist/Ex430.java
@@ -0,0 +1,53 @@
+package leetcode.linkedlist;
+
+/**
+ * @author yuzhang
+ * @date 2020/7/3 6:01 下午
+ * 扁平化链表
+ */
+public class Ex430 {
+    public static void main(String[] args) {
+
+    }
+
+    public Node flatten(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node newHead = new Node();
+        newHead.next = head;
+        dfs(newHead, head);
+        newHead.next.pre = null;
+        return newHead.next;
+    }
+
+    private Node dfs(Node prev, Node cur) {
+        if (cur == null) {
+            return prev;
+        }
+        // 将child与prev通过next关联起来
+        prev.next = cur;
+        cur.pre = prev;
+        Node tempNext = cur.next;
+        // 将该链表看成一个二叉树，child为左子树
+        Node tail = dfs(cur, cur.child);
+        cur.child = null;
+        // next为右子树
+        return dfs(tail, tempNext);
+    }
+
+    static class Node {
+        int val;
+        Node next;
+        Node pre;
+        Node child;
+
+        public Node() {
+        }
+
+        public Node(int val, Node pre) {
+            this.val = val;
+            this.pre = pre;
+        }
+    }
+}
---
+++ src/main/java/leetcode/linkedlist/Ex876.java
@@ -0,0 +1,22 @@
+package leetcode.linkedlist;
+
+/**
+ * @author yuzhang
+ * @date 2020/7/3 11:24 上午
+ * 链表的中间结点
+ */
+public class Ex876 {
+    public ListNode middleNode(ListNode head) {
+        if (head==null){
+            return null;
+        }
+        // 快慢指针
+        ListNode slower=head;
+        ListNode faster=head;
+        while(faster!=null&&faster.next!=null){
+            slower=slower.next;
+            faster=faster.next.next;
+        }
+        return slower;
+    }
+}
