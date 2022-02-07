98daffcaa876bff7963a7ca15fe734f555ad2660
zhangyu@itiger.com <zhangyu@itiger.com>
2022-02-07 10:19:21 +0800

    练习2022.1.24~2022.1.30

---
+++ src/main/java/lcp/Ex30.java
@@ -0,0 +1,41 @@
+package lcp;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author yu zhang
+ */
+public class Ex30 {
+    public static void main(String[] args) {
+        int[] nums = {-1, -1, 10};
+        System.out.println(new Ex30().magicTower(nums));
+    }
+    public int magicTower(int[] nums) {
+        if (Arrays.stream(nums).sum() < 0) {
+            return -1;
+        }
+        long sum = 0, right = 0;
+        int counter = 0;
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            que.offer(nums[i]);
+            sum += nums[i];
+            if (sum >= 0) {
+                continue;
+            }
+            while (!que.isEmpty() && que.peek() < 0 && sum < 0) {
+                int poll = que.poll();
+                right += poll;
+                sum = sum - poll;
+                counter++;
+            }
+            if (sum < 0) {
+                return -1;
+            }
+        }
+        return sum + right < 0 ? -1 : counter;
+    }
+}
---
+++ src/main/java/leetcode/hash/Ex2013.java
@@ -0,0 +1,75 @@
+package leetcode.hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author yu zhang
+ */
+public class Ex2013 {
+    public static void main(String[] args) {
+        Ex2013 ex2013 = new Ex2013();
+        ex2013.add(new int[]{5, 10});
+        ex2013.add(new int[]{10, 5});
+        ex2013.add(new int[]{10, 10});
+        System.out.println(ex2013.count(new int[]{5, 5}));
+        ex2013.add(new int[]{3, 0});
+        ex2013.add(new int[]{8, 0});
+        ex2013.add(new int[]{8, 5});
+        System.out.println(ex2013.count(new int[]{3, 5}));
+        ex2013.add(new int[]{9, 0});
+        ex2013.add(new int[]{9, 8});
+        ex2013.add(new int[]{1, 8});
+        System.out.println(ex2013.count(new int[]{1, 0}));
+        ex2013.add(new int[]{0, 0});
+        ex2013.add(new int[]{8, 0});
+        ex2013.add(new int[]{8, 8});
+        System.out.println(ex2013.count(new int[]{0, 8}));
+    }
+
+    /**
+     * <y, <x, 坐标为(x,y)的个数>>
+     */
+    private HashMap<Integer, Map<Integer, Integer>> cntMap;
+
+    public Ex2013() {
+        cntMap = new HashMap<>();
+    }
+
+    public void add(int[] point) {
+        Map<Integer, Integer> xMap = cntMap.computeIfAbsent(point[1], key -> new HashMap<>());
+        xMap.put(point[0], xMap.getOrDefault(point[0], 0) + 1);
+    }
+
+    public int count(int[] point) {
+        int x = point[0], y = point[1];
+        Map<Integer, Integer> xMap = cntMap.get(y);
+        if (xMap == null) {
+            return 0;
+        }
+        int count = 0;
+        // 和point相同纵坐标的点
+        for (Map.Entry<Integer, Integer> entry: xMap.entrySet()) {
+            int xAxis = entry.getKey();
+            if (xAxis == x) {
+                continue;
+            }
+            // 长度
+            int distance = x - xAxis;
+            // 向上找
+            count += entry.getValue() // (xAxis, y)
+                    // (x, y+d)
+                    * cntMap.getOrDefault(y + distance, new HashMap<>()).getOrDefault(x, 0)
+                    // (xAxis, y+d)
+                    * cntMap.getOrDefault(y + distance, new HashMap<>()).getOrDefault(xAxis, 0);
+            count += entry.getValue() // (xAxis, y)
+                    // (x, y - d)
+                    * cntMap.getOrDefault(y - distance, new HashMap<>()).getOrDefault(x, 0)
+                    // (xAxis, y-d)
+                    * cntMap.getOrDefault(y - distance, new HashMap<>()).getOrDefault(xAxis, 0);
+        }
+        return count;
+    }
+}
