b242983b3ea074acd680a48993b4b34acaf99c69
yuzhang <18681820448@163.com>
2022-05-04 20:15:33 +0800

    练习2022.5.02~2022.5.08

--- src/main/java/leetcode/tree/TreeNode.java
+++ src/main/java/leetcode/tree/TreeNode.java
@@ -4,12 +4,12 @@ package leetcode.tree;
  * @author yu zhang
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
