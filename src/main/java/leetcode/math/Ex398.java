40d5b83d3b1fa493ced2a58b1ac7f19fe5c5ecaf
zhangyu@itiger.com <zhangyu@itiger.com>
2022-06-11 14:28:13 +0800

    2022.06.06~2022.06.12练习

--- src/main/java/leetcode/math/Ex398.java
+++ src/main/java/leetcode/math/Ex398.java
@@ -1,4 +1,4 @@
-package leetcode.hash;
+package leetcode.math;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/java/leetcode/string/Ex165.java
+++ src/main/java/leetcode/string/Ex165.java
@@ -6,17 +6,27 @@ package leetcode.string;
  * TODO
  */
 public class Ex165 {
-    /*public int compareVersion(String version1, String version2) {
-        String[] versions1=version1.split("\\.");
-        String[] versions2=version2.split("\\.");
+    public static void main(String[] args) {
+        System.out.println(new Ex165().compareVersion("1.05", "1.1"));
+    }
 
-        int i = 0,j=0;
-        for (;i<version1.length()&&j<version2.length();i++,j++){
-            while("0".equals(versions1[i++])){
-                if (i==version1.length()-1){
-                    break;
-                }
+    public int compareVersion(String version1, String version2) {
+        String[] versions1 = version1.split("\\."), versions2 = version2.split("\\.");
+        int len1 = versions1.length, len2 = versions2.length;
+        for (int i = 0; i < len1 || i < len2; i++) {
+            int num1 = 0, num2 = 0;
+            if (i < len1) {
+                num1 = Integer.parseInt(versions1[i]);
+            }
+            if (i < len2) {
+                num2 = Integer.parseInt(versions2[i]);
+            }
+            if (num1 > num2) {
+                return 1;
+            } else if (num1 < num2) {
+                return -1;
             }
         }
-    }*/
+        return 0;
+    }
 }
