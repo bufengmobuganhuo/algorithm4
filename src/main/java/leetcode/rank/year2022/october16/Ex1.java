51a4309e0edb5d13155cc344212a956a9fef18b0
yuzhang <18681820448@163.com>
2022-10-30 09:47:07 +0800

    2022.09.16练习

---
+++ src/main/java/leetcode/rank/year2022/october16/Ex1.java
@@ -0,0 +1,25 @@
+package leetcode.rank.year2022.october16;
+
+import java.util.Arrays;
+
+/**
+ * @author yuzhang
+ * @date 2022/10/16 10:08
+ * TODO
+ */
+public class Ex1 {
+    public int findMaxK(int[] nums) {
+        Arrays.sort(nums);
+        int leftPtr = 0, rightPtr = nums.length - 1;
+        while (leftPtr < rightPtr) {
+            if (nums[leftPtr] + nums[rightPtr] == 0) {
+                return nums[rightPtr];
+            } else if (-nums[leftPtr] > nums[rightPtr]) {
+                leftPtr++;
+            } else {
+                rightPtr--;
+            }
+        }
+        return -1;
+    }
+}
