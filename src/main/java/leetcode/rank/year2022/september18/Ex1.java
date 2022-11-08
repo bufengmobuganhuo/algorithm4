3c1568e15cd7963718b0017d4827284c2cec8484
zhangyu@itiger.com <zhangyu@itiger.com>
2022-11-08 17:05:29 +0800

    2022.09.08练习

---
+++ src/main/java/leetcode/rank/year2022/september18/Ex1.java
@@ -0,0 +1,13 @@
+package leetcode.rank.year2022.september18;
+
+/**
+ * @author yu zhang
+ */
+public class Ex1 {
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        }
+        return n * 2;
+    }
+}
