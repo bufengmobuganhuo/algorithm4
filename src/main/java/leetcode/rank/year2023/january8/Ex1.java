6f76f07a359ec50b25a1b9f854054862bcef9c64
yuzhang <18681820448@163.com>
2023-01-08 19:35:37 +0800

    2023.01.08比赛

---
+++ src/main/java/leetcode/rank/year2023/january8/Ex1.java
@@ -0,0 +1,20 @@
+package leetcode.rank.year2023.january8;
+
+/**
+ * @author yuzhang
+ * @date 2023/1/8 10:07
+ * TODO
+ */
+public class Ex1 {
+    public int maximumCount(int[] nums) {
+        int pos = 0, neg = 0;
+        for (int num : nums) {
+            if (num > 0) {
+                pos++;
+            } else if (num < 0) {
+                neg++;
+            }
+        }
+        return Math.max(pos, neg);
+    }
+}
